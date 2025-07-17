4bba12b2ed4bb438fa3bf922f3f2cd6a8dd11027
Alsabbagh Louai <144901821+louals@users.noreply.github.com>
2025-07-16 20:48:14 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI
 from app.routes import generator
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or "*" to allow all for dev
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(generator.router, prefix="/question-gen")
